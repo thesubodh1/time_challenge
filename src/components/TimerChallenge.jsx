98fda48b25cebf5b314ee55c3e34ee35e1b3004d
thesubodh1 <thesubodh1@gmail.com>
2025-09-27 20:18:27 +0545

    More UI features added

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 export default function TimerChallenge({title,targetTime}) {
+    const [timerExpired,setTimerExpired] = useState(false);
+    const[timerStarted,setTimerStarted]=useState(false);
+
+    function handleStart() {
+        setTimerStarted(true);
+        setTimeout(() => {setTimerExpired(true)},targetTime*1000)
+    }
 
   return <section className="challenge">
     <h2>{title}</h2>
+    {timerExpired && <p>You Lost!</p>}
     <p className="challenge-time">
         {targetTime} second{targetTime>1?'s':''}
     </p>
-    <button>Start Challenge</button>
-    <p className="">Time is Running .....</p>
+    <button onClick={handleStart}>{timerStarted ? 'Stop':'Started'} Challenge</button>
+    <p className={timerStarted ? 'active':undefined}>{timerStarted ? "Time is Running .....":"Timer Inactive"}</p>
   </section>;
 }
